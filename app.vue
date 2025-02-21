7ef8bc744f351a4862131cc6bf33afcbbd89961f
adilet-myrzabaev <a.s.myrzabaev@gmail.com>
2025-02-21 11:22:27 +0600

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <div class="app">
+        <NuxtPage />
+      </div>
+    </NuxtLayout>
+
+  </div>
+</template>
+<script setup lang="js">
+import("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js");
+</script>
+<style>
+@import url("/assets/styles/style.css");
+.app{
+  min-height: 100vh;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  modules: ['@pinia/nuxt'],
+  plugins: [
+      '~/plugins/vue-the-mask.ts',
+      '~/plugins/autoLogin.ts',
+  ],
+  css: ["bootstrap/dist/css/bootstrap.min.css"]
+})
