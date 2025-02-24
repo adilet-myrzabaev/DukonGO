ed26d347e7946db8d097068e91ce7191916eb820
shagent007 <shagent118@gmail.com>
2025-02-24 10:54:53 +0600

    asdasd

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,7 +2,9 @@ import { defineNuxtRouteMiddleware, navigateTo } from '#app';
 
 export default defineNuxtRouteMiddleware((to, from) => {
     const authStore = useAuthStore();
-
+    authStore.tryLogin();
+    console.log(authStore.isAuthenticate );
+    
     if (authStore.isAuthenticate || to.path == '/login/signin') {
         console.log('not logged in', to.fullPath)
         return;
